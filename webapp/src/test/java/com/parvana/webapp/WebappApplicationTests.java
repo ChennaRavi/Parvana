46d31df4cd89a10a7f54fcebdccb707609e96cae
SimplilearnProjectSubmission-FullStack <testacnt562@gmail.com>
2023-09-03 18:38:03 +0530

    Pushing the webapp into repo

---
+++ webapp/src/test/java/com/parvana/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parvana.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
